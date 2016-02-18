d9ea47b3ade8143c4412e91c09a25170911242a8
hqnam <1259025@itec.hcmus.edu.vn>
2016-02-19 00:49:49 +0700

    Source

---
+++ Project/Project/Controllers/LogonController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class LogonController : Controller
+    {
+        // GET: Logon
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        
+    }
+}
