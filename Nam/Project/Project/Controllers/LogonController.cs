a0407019af7f8569ac9ce875add6ed96fb22ec32
hqnam <1259025@itec.hcmus.edu.vn>
2016-02-25 22:30:41 +0700

    Nam's Folder

---
+++ Nam/Project/Project/Controllers/LogonController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class LogonController : Controller
+    {
+        QLPhongKhamEntities1 db = new QLPhongKhamEntities1();
+
+        // GET: Logon
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+        [HttpPost ]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy(User us)
+        {
+            if (ModelState.IsValid)
+            {
+                // chèn dữ liệu vào bảng Users
+                db.Users.Add(us);
+                // lưu vào CSDL
+                db.SaveChanges();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string user = f.Get("txtTaiKhoan").ToString();
+            string pwd = f.Get("txtMatKhau").ToString();
+            User us = db.Users.SingleOrDefault(n => n.Name == user && n.Pwd == pwd);
+            if(us != null)
+            {
+                ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                Session["Username"] = us;
+                return View();
+            }
+            ViewBag.ThongBao = "Tên tài khoản hoặc mật khẩu không đúng";
+            return View();
+        }
+    }
+}
---
+++ Nam/Project/Project/Controllers/ManageClinicController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class ManageClinicController : Controller
+    {
+        // GET: ManageClinic
+        QLPhongKhamEntities1 db = new QLPhongKhamEntities1();
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        //Create User
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //ViewBag.IDGroup = db.Users.ToList();
+            //ViewBag.IDGroup = db.GroupUsers.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User us)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Users.Add(us);
+                db.SaveChanges();
+            }
+            return View();
+        }
+
+        //Edit User
+        //[HttpPost]
+        //public ActionResult Edit(int id)
+        //{
+        //    User us = db.Users.SingleOrDefault(n => n.ID = id);
+
+        //    return View();
+        //}
+    }
+}
